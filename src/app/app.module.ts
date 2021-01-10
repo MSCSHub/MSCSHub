3d2a6418d3c69c92732112931569d5c2c36c8da1
Eric <eric@unknown183eefea77ee.attlocal.net>
2021-01-10 16:25:14 -0600

    Updated nav bar, created search-input, created theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MatIconModule } from '@angular/material/icon'
+import { MaterialModule } from './material/material.module'
+import { HttpClientModule } from '@angular/common/http';
+import { SearchInputComponent } from './search/search-input/search-input.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    NavbarComponent
+    NavbarComponent,
+    SearchInputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -12,4 +12,7 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  doSearch(event: any){
+    
+  }
 }
