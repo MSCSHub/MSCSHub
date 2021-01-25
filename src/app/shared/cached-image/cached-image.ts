4e74b85068802adc4156b9dd46c2909c644cd63d
Eric <eric@Erics-MacBook-Air.local>
2021-01-24 18:36:49 -0600

    Auto-generated commit

--- src/app/shared/cached-image/cached-image.ts
+++
@@ -1,4 +0,0 @@
-export interface CachedImage {
-  url: string,
-  blob: Blob,
-}
--- src/app/shared/review/review.ts
+++
@@ -1,9 +0,0 @@
-export interface Review {
-  BookUsefulness: number,
-  ClassId: string,
-  Difficulty: number,
-  Rating: number,
-  Review: string,
-  Workload: number,
-  Timestamp?: Date
-}
