12f65c230bd740088f5f887395d0fc884d9afec6
EricPryzant <eric.pryzant@gmail.com>
2021-05-11 20:17:09 -0500

    Improved filtering to do a set intersection instead of a union

--- src/app/courses/course-grid/course-grid.component.ts
+++ src/app/courses/course-grid/course-grid.component.ts
@@ -14,6 +14,8 @@ export class CourseGridComponent implements OnInit {
   selected: string[] = []
   visibleClasses: ClassData[] = []
   visibleClassesSet: Set<ClassData> = new Set()
+  selectedLangs: Set<ClassData> = new Set()
+  selectedCats: Set<ClassData> = new Set()
 
   constructor(
     private courses: ClassService,
@@ -46,38 +48,41 @@ export class CourseGridComponent implements OnInit {
   }
 
   filterItems(): void {
-    // this.visibleClasses = []
     this.visibleClassesSet.clear()
-    let selectedLangs: ClassData[] = []
-    let selectedCats: ClassData[] = []
-    if(this.selected.length === 0){
-      this.classes.forEach(data => {
+    this.selectedCats.clear()
+    this.selectedLangs.clear()
+    // We switched from a list to a set because we were seeing duplicates. Using a set ensures entries are unique
+    if(this.selected.length === 0){                                                 // If selected is empty
+      this.classes.forEach(data => {                                                // Add every class to the visible set
         this.visibleClassesSet.add(data)
       })
-    } else {
-      for(var i=0; i<this.selected.length; i++){
-        let item = this.selected[i]
-        if(this.categories.includes(item)){
-          this.classes.filter(x => x.category === item).forEach(data => {
-            this.visibleClassesSet.add(data)
+    } else {                                                                        // Something is selected
+      for(var i=0; i<this.selected.length; i++){                                    // For every item in this.selected
+        let item = this.selected[i]                                                 // Get selection to process
+        if(this.categories.includes(item)){                                         // Check if it's a category
+          this.classes.filter(x => x.category === item).forEach(data => {           // Look for classes with categories that match the chosen cat
+            this.selectedCats.add(data)                                              // Add it to the set
           })
-        } else {
-          this.classes.filter(x => x.languages?.includes(item)).forEach(data => {
-            this.visibleClassesSet.add(data)
+        } else {                                                                    // Otherwise it's a language
+          this.classes.filter(x => x.languages?.includes(item)).forEach(data => {   // Look for classes with a language that matches the chosen lang
+            this.selectedLangs.add(data)                                            // Add it to the set
           })
         }
       }
-      if (selectedLangs.length === 0 || selectedCats.length === 0){
-        selectedCats.forEach(data => {
-          this.visibleClassesSet.add(data)
-        })
+      if (this.selectedLangs.size === 0 || this.selectedCats.size === 0){           // If only one field is selected then 
+        this.selectedCats.forEach(data => {                                         // Add everything in selectedCats
+          this.visibleClassesSet.add(data)                                          
+        })                                                                          
+        this.selectedLangs.forEach(data => {                                        // Add everything in selectedLangs
+          this.visibleClassesSet.add(data)                                          // It looks like we're doing too much work but really one has to be empty
+        })                                                                          // Instead of branching we can just do both operations and not care
       } else {
-        selectedLangs.filter(x => selectedCats.includes(x)).forEach(data => {
-          this.visibleClassesSet.add(data)
+        [...this.selectedLangs].filter(x => this.selectedCats.has(x)).forEach(data => {   // Convert selectedLangs to a list and filter out everything not in selectedCats
+          this.visibleClassesSet.add(data)                                                // Add the intersection of the two sets to the visible classes set
         })
       }
     }
-    this.visibleClasses = Array.from(this.visibleClassesSet)
-    this.visibleClasses.sort((a, b) => (a.ClassName > b.ClassName) ? 1 : -1)
+    this.visibleClasses = Array.from(this.visibleClassesSet)                        // Convert the set into an array
+    this.visibleClasses.sort((a, b) => (a.ClassName > b.ClassName) ? 1 : -1)        // Sort the list alphabetically
   }
 }
